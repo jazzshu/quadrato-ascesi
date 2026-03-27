899a936b5465bb170d3d1623ec443a5a022aff19
jazzmatazz <50492920+jazzshu@users.noreply.github.com>
2026-03-27 17:18:34 +0000

    fix vite entry path for github pages and component import

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import QuadratoCalculator from './components/QuadratoCalculator'
+import QuadratoCalculator from './QuadratoCalculator'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
